d4c38c1116a38196d2b042379775937122d37dd4
ShivamTripathi-sudo <shivamtripathi6060@gmail.com>
2024-09-21 10:34:28 +0530

    done

--- .devcontainer/01_Basics/array.js
+++ .devcontainer/01_Basics/array.js
@@ -1,3 +1,22 @@
 var start=[ 'shyam' ,'hari','bhola']
 console.log(start)
 console.log(start[2])
+let penut='penut is like that almond'
+var ot="correct"
+var dob = "30-12-2000"
+var gender = "male"
+let age = 25
+const notChange = 984
+var cons = String(notChange)
+console.log(ot)
+ var accountId = "2003510100064"
+ console.table([penut ,ot, dob ,gender,accountId,notChange])
+
+ 
+//  you can find which type of datatype you written inside console.log(typeof nameof the variable)
+
+
+ console.log(typeof age)
+ console.log(typeof notChange)
+ console.log(typeof cons)
+ console.log(cons)
