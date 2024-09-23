be2783dc580b5ad67950d6eafda9ac2b9fc6568f
ShivamTripathi-sudo <shivamtripathi6060@gmail.com>
2024-09-23 09:54:11 +0530

    done

---
+++ .devcontainer/01_Basics/Basicinformation.js
@@ -0,0 +1,45 @@
+// treat all js code as newer version
+"use strict";
+
+// PRIMITIVE DATA TYPE
+
+/* alert(3+2) ******** we are using nodejs , not browser
+DATTYPES
+number
+string
+boolean
+null= standalone value 
+undefined= true/false
+*/
+var a= 40
+var b= 'google'
+// console.log(a)
+// console.log(typeof a)
+// console.log(b)
+// console.log(typeof b)
+// var changedataype= String(b)
+// console.log(changedataype)
+// NaN not a number
+// let hi = null
+// console.log(hi)
+// console.log(typeof hi )
+// console.log(typeof (hi))
+// var float = 'hello'
+// var change= Number(float)
+// console.log(change)
+// console.log(typeof change)
+// console.log(change)
+// let login= '0'   
+// somthing is written inside cot is output is true and nothing is there output is false
+// let boolean=Boolean(login)
+// console.log(boolean)
+// console.log(typeof login)
+// console.log(login)
+let number = undefined
+console.log(typeof number)
+let numbertostring = Number(number)
+// console.log(typeof numbertostring)
+console.log(numbertostring)
+
+
+
--- .devcontainer/01_Basics/array.js
+++ .devcontainer/01_Basics/array.js
@@ -1,22 +1,30 @@
-var start=[ 'shyam' ,'hari','bhola']
-console.log(start)
-console.log(start[2])
-let penut='penut is like that almond'
-var ot="correct"
-var dob = "30-12-2000"
-var gender = "male"
-let age = 25
-const notChange = 984
-var cons = String(notChange)
-console.log(ot)
- var accountId = "2003510100064"
- console.table([penut ,ot, dob ,gender,accountId,notChange])
+// var start=[ 'shyam' ,'hari','bhola']
+// console.log(start)
+// console.log(start[2])
+// let penut='penut is like that almond'
+// var ot="correct"
+// var dob = "30-12-2000"
+// var gender = "male"
+// let age = 25
+// const notChange = 984
+// var cons = String(notChange)
+// console.log(ot)
+//  var accountId = "2003510100064"
+//  console.table([penut ,ot, dob ,gender,accountId,notChange])
 
- 
+
 //  you can find which type of datatype you written inside console.log(typeof nameof the variable)
 
 
- console.log(typeof age)
- console.log(typeof notChange)
- console.log(typeof cons)
- console.log(cons)
+//  console.log(typeof age)
+//  console.log(typeof notChange)
+//  console.log(typeof cons)
+//  console.log(cons)
+// console.log(typeof null);
+// console.log(typeof undefined)
+// console.log(typeof string)
+// console.log(typeof number)
+let login=""
+let boolean = Boolean(login)
+// console.log(boolean);
+console.log(login);
