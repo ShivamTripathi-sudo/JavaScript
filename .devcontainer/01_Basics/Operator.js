26a023732f92d17fdad6c7afe9e7c9b729a96169
ShivamTripathi-sudo <shivamtripathi6060@gmail.com>
2024-09-25 11:24:22 +0530

    operator

---
+++ .devcontainer/01_Basics/Operator.js
@@ -0,0 +1,18 @@
+// let value= 10;
+// let value1= -value;
+// console.log(value1);
+// console.log(2**4);
+// console.log(2%10);
+// console.log(3.0%12.5);
+// console.log('1'+ 2+2)
+// console.log(2+2+3+"2")
+// console.log(2/0);
+"use strict";
+// console.log(null=0);
+console.log(null>=0);
+console.log(null==0)
+console.log(null<=0)
+console.log(null)
+
+
+
