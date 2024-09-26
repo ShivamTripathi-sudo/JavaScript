8afe191dbf1cea0c51d4e9170effd736dbcb407c
ShivamTripathi-sudo <shivamtripathi6060@gmail.com>
2024-09-26 19:15:32 +0530

    Strings

---
+++ 02_datatypes/Strings.js
@@ -0,0 +1,18 @@
+const name1 = "keshav tripathi";
+let age = 23;
+const color = "fair";
+console.log(name1 + " " + age);
+console.log(
+  `hello my name is ${name1} and my age is ${age} and color is ${color} `
+);
+const sirname = new String("shivamtripathi");
+// console.log(sirname[6]);
+// console.log(sirname.__proto__.__proto__);
+// console.log(sirname.length);
+// console.log(sirname.toUpperCase());
+// console.log(sirname.charAt(2));
+// console.log(sirname.lastIndexOf("t"));
+// console.log(sirname.indexOf("v"));
+console.log(sirname.substring(0, 4));
+const anotherString = sirname.slice(-8, 0);
+console.log(anotherString);
