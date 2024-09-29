c37cfb947b4c84d10f785051e4234bc619634a66
ShivamTripathi-sudo <shivamtripathi6060@gmail.com>
2024-09-29 11:20:17 +0530

    Date update

--- 04_dates/date.js
+++ 04_dates/date.js
@@ -1,6 +1,24 @@
-let myDate = new Date();
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toISOString());
-console.log(myDate.getDate());
-console.log(typeof myDate);
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.getDate());
+// console.log(typeof myDate);
+
+// let myCreatedate = new Date(2024, 0, 23);
+// console.log(myCreatedate.toDateString());
+// let myCreatedate1 = new Date(2024, 5, 23, 4, 7);
+// console.log(myCreatedate1.toDateString());
+// console.log(myCreatedate1.toLocaleDateString());
+// console.log(myCreatedate1.toLocaleString());
+
+// let mytymeStamp = Date.now();
+// console.log(mytymeStamp);
+// console.log(myCreatedate1.getTime());
+
+let newdate = new Date();
+
+console.log(newdate);
+console.log(newdate.getDate() + 1);
+console.log(newdate.getDay());
+console.log(Date);
